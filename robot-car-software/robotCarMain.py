062cf67d97184ef1e003869f9f3ecb94334f59dd
tnkyk <yuukiyuuki327@gmail.com>
2026-01-02 14:44:01 +0900

    keyboard input move & a few fix

--- robot-car-software/robotCarMain.py
+++ robot-car-software/robotCarMain.py
@@ -1,20 +1,38 @@
-from pimygpio import PI_GPIO
 from dcMortorL298N import Motor
+import sys
+import termios
+import tty
+
+def getch():
+    """1文字だけ即時取得する関数（Enter不要）"""
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(fd)  # 端末をrawモードに（エコーもオフ）
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)  # 端末設定を復元
+    return ch
 
 def main():
     dcMotor = Motor()
     # キーボードの入力を取得してモーターを制御する
     while True:
-        key = input("Enter command (w: forward, s: backward, a: left, d: right, q: quit): ")
+        print("w:前進、s:後進、a:左、d:右、e:ストップ、'q' でプログラム終了。")
+        key = getch()
         if key == 'w':
-            dcMotor.forward()
+            print("click FORWARD")
+            dcMotor.move(Motor.FORWARD,20)
         elif key == 's':
-            dcMotor.backward()
+            dcMotor.move(Motor.BACKWARD,20)
         elif key == 'a':
-            dcMotor.left()
+            dcMotor.move(Motor.LEFT,20)
         elif key == 'd':
-            dcMotor.right()
+            dcMotor.move(Motor.RIGHT,20)
+        elif key == 'e':
+            dcMotor.move(Motor.STOP,0)
         elif key == 'q':
+            dcMotor.move(Motor.STOP,0)
             break
         else:
             print("Invalid command")
