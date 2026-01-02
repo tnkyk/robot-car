3f31489f9df1b7775dd02a86e3a9b2cdf6767d15
tnkyk <yuukiyuuki327@gmail.com>
2026-01-02 13:59:59 +0900

    DCモーターテスト用にキーボード操作で動くプログラムの追加

--- robot-car-software/robotCarMain.py
+++ robot-car-software/robotCarMain.py
@@ -3,7 +3,21 @@ from dcMortorL298N import Motor
 
 def main():
     dcMotor = Motor()
-    
+    # キーボードの入力を取得してモーターを制御する
+    while True:
+        key = input("Enter command (w: forward, s: backward, a: left, d: right, q: quit): ")
+        if key == 'w':
+            dcMotor.forward()
+        elif key == 's':
+            dcMotor.backward()
+        elif key == 'a':
+            dcMotor.left()
+        elif key == 'd':
+            dcMotor.right()
+        elif key == 'q':
+            break
+        else:
+            print("Invalid command")
 
 if __name__ == "__main__":
     main()
